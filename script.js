1a576ecd030a2c5685a6529ce31c175a859116a5
‘Katy’ <katyinseattle@me.com>
2017-02-11 17:42:39 -0800

    Scaffolding

---
+++ script.js
@@ -0,0 +1,39 @@
+//Store minimum and max hourly customers, average cookie per customer
+
+var custPerHourPikePlace{
+  min: 17,
+  max: 88,
+  averageCookies: 5.2
+};
+
+var custPerHourSeaTac{
+  min: 6,
+  max: 24,
+};
+
+var custPerHourSouthcenter{
+  min: 11,
+  max: 38,
+};
+
+var custPerHourBellSquare{
+  min: 20,
+  max: 48,
+};
+
+var custPerHourAlki{
+  min: 3,
+  max: 24,
+};
+
+// Use a method of the object to generate a random number of customers per hour (math.random)
+
+
+// Calculate simulated amounts of cookie purchased
+
+
+// Store results for each location in a seperate array
+
+// Display the values of each array as unordered lists in browser
+
+// Calculate the sum of the hourly totals - display on index.html
