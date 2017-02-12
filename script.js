9c23e11c5b09516396cd657583bdba5a16a3721f
‘Katy’ <katyinseattle@me.com>
2017-02-11 18:26:48 -0800

    added method generating random number of customers

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-//Store minimum and max hourly customers, average cookie per customer
-
-var custPerHourPikePlace{
+//Store minimum and max hourly customers, average cookie per customer in an object
+var customersPikePlace{
   min: 17,
   max: 88,
   averageCookies: 5.2
+
+  custPerHour: function() {
+    return Math.random() * (max - min) + min;
+  }
 };
 
-var custPerHourSeaTac{
+var customersHourSeaTac{
   min: 6,
   max: 24,
+  averageCookies: 1.2,
+
+  custPerHour: function() {
+    return Math.random() * (max - min) + min;
+  }
 };
 
-var custPerHourSouthcenter{
+var customersSouthcenter{
   min: 11,
   max: 38,
+  averageCookies: 1.9,
+
+  custPerHour: function() {
+    return Math.random() * (max - min) + min;
+  }
 };
 
-var custPerHourBellSquare{
+var customersBellSquare{
   min: 20,
   max: 48,
+  averageCookies: 3.3,
+
+  custPerHour: function() {
+    return Math.random() * (max - min) + min;
+  }
 };
 
-var custPerHourAlki{
+var customersAlki{
   min: 3,
   max: 24,
+  averageCookies: 2.6,
+
+  custPerHour: function() {
+    return Math.random() * (max - min) + min;
+  }
 };
 
 // Use a method of the object to generate a random number of customers per hour (math.random)
